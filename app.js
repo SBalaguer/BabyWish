921b3d2d77e22f86116bf43b0665ee84c55d0454
Santi Balaguer <santiago.balaguer@gmail.com>
2019-12-12 14:45:21 +0000

    passport configuration

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-'use strict';
+"use strict";
 
-const { join } = require('path');
-const express = require('express');
-const createError = require('http-errors');
-const connectMongo = require('connect-mongo');
-const cookieParser = require('cookie-parser');
-const expressSession = require('express-session');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const serveFavicon = require('serve-favicon');
-const bindUserToViewLocals = require('./middleware/bind-user-to-view-locals.js');
-const passportConfigure = require('./passport-configuration.js');
+const { join } = require("path");
+const express = require("express");
+const createError = require("http-errors");
+const connectMongo = require("connect-mongo");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const serveFavicon = require("serve-favicon");
+const bindUserToViewLocals = require("./middleware/bind-user-to-view-locals.js");
+const passportConfigure = require("./passport-configuration.js");
 
 // DEFINE ROUTERS HERE
-const indexRouter = require('./routes/index');
-const authenticationRouter = require('./routes/authentication');
-const userApiRouter = require('./routes/api/userApi');
+const indexRouter = require("./routes/index");
+const authenticationRouter = require("./routes/authentication");
+const userApiRouter = require("./routes/api/userApi");
 
 const app = express();
 
-app.use(serveFavicon(join(__dirname, 'public/images', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(serveFavicon(join(__dirname, "public/images", "favicon.ico")));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -36,9 +36,9 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 60 * 60 * 24 * 15,
-      sameSite: 'lax',
+      sameSite: "lax",
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production'
+      secure: process.env.NODE_ENV === "production"
     },
     store: new (connectMongo(expressSession))({
       mongooseConnection: mongoose.connection,
@@ -46,14 +46,15 @@ app.use(
     })
   })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bindUserToViewLocals);
 
 // DEFINE PATH ROUTES HERE
-app.use('/authentication', authenticationRouter);
-app.use('/api/user', userApiRouter);
-app.use('/', indexRouter);
+app.use("/authentication", authenticationRouter);
+app.use("/api/user", userApiRouter);
+app.use("/", indexRouter);
 
 // Catch missing routes and forward to error handler
 app.use((req, res, next) => {
@@ -64,10 +65,10 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   // Set error information, with stack only available in development
   res.locals.message = error.message;
-  res.locals.error = req.app.get('env') === 'development' ? error : {};
+  res.locals.error = req.app.get("env") === "development" ? error : {};
 
   res.status(error.status || 500);
-  res.json({ type: 'error', error: { message: error.message } });
+  res.json({ type: "error", error: { message: error.message } });
 });
 
 module.exports = app;
