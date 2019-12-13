82a5e22347a45d0dc2e631c45513debbb5748cb8
Santi Balaguer <santiago.balaguer@gmail.com>
2019-12-13 12:59:06 +0000

    added products and single product view routes and folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,9 @@ import SignUpThreeGifter from "./Views/SignUp/SignUpThreeGifter";
 import SignIn from "./Views/SignIn";
 import Navbar from "./Components/Navbar";
 import WishList from "./Views/WishList";
+import ProfileView from "./Views/ProfileView";
+import Products from "./Views/Products";
+import SingleProductView from "./Views/SingleProductView";
 
 class App extends Component {
   constructor(props) {
@@ -111,7 +114,29 @@ class App extends Component {
           <Route
             path="/user/:id"
             render={props => (
-              <SignIn
+              <ProfileView
+                updateUserState={this.updateUserState}
+                userState={this.state.user}
+                addUsertoUserState={this.addUsertoUserState}
+                {...props}
+              />
+            )}
+          />
+          <Route
+            path="/products/:id"
+            render={props => (
+              <SingleProductView
+                updateUserState={this.updateUserState}
+                userState={this.state.user}
+                addUsertoUserState={this.addUsertoUserState}
+                {...props}
+              />
+            )}
+          />
+          <Route
+            path="/products/"
+            render={props => (
+              <Products
                 updateUserState={this.updateUserState}
                 userState={this.state.user}
                 addUsertoUserState={this.addUsertoUserState}
