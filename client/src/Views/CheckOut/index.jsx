d21a6f3f234ab0d042a46c5f75c68f526828a55d
Santi Balaguer <santiago.balaguer@gmail.com>
2019-12-15 13:27:20 +0000

    added a new test to checkout

--- client/src/Views/CheckOut/index.jsx
+++ client/src/Views/CheckOut/index.jsx
@@ -8,12 +8,25 @@ export class CheckOut extends Component {
     super (props)
     this.state={
       product: {
-        productPrice: 4,
+        productPrice: 0,
         //IMPORTANT: We need to send product price in CENTS to Stripe
         name: 'Test'
-      }
+      },
+      status: false
     }
     this.handleToken = this.handleToken.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange(event){
+    const value = event.target.value
+    console.log(value)
+    this.setState({
+      product:{
+        name: 'Test',
+        productPrice: value
+      }
+    })
   }
 
   async handleToken(token){
@@ -22,6 +35,7 @@ export class CheckOut extends Component {
     try{
       const status = await processPayment(token, product)
       console.log(status)
+      this.setState({status:true})
     } catch (error) {
       console.log(error)
     }
@@ -31,13 +45,19 @@ export class CheckOut extends Component {
     return (
       <div>
         <h1>Checkout Page</h1>
+        <h5>Select amout to Pay</h5>
+        <input type="number" name="productPrice" value={this.state.product.productPrice} onChange={this.handleInputChange}/>
+        
         <StripeCheckout 
           stripeKey = {stripePublicApiKey}
           token = {this.handleToken}
           billingAddress
           amount = {this.state.product.productPrice * 100}
           name = {this.state.product.name}
-        />
+        >
+          <button className='btn btn-start btn-block'>Pay Baby Wish!</button>
+        </StripeCheckout>
+
       </div>
     )
   }
