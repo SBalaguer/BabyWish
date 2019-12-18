572a3c0f6ee0d77f7a4c1c9e7c7869638d63d6c4
Santi Balaguer <santiago.balaguer@gmail.com>
2019-12-18 08:45:21 +0000

    shopping cart routes and services

--- client/src/services/checkout.js
+++ client/src/services/checkout.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const processPayment = async (token,product) => {
+export const processPayment = async (token, product) => {
   try {
-    const response = await axios.post("/api/checkout", {token, product});
-    return response.data.status
+    const response = await axios.post("/api/checkout", { token, product });
+    return response.data.status;
     //this will return success or failure
   } catch (error) {
     throw error;
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
-const { Router } = require('express');
+const { Router } = require("express");
 
-const passport = require('passport');
+const passport = require("passport");
 
 const router = new Router();
 
 router.post(
-  '/sign-in',
-  passport.authenticate('local-sign-in'),
+  "/sign-in",
+  passport.authenticate("local-sign-in"),
   (req, res, next) => {
     const user = req.user;
     res.json({ user });
   }
 );
 
-router.post('/sign-out', (req, res, next) => {
+router.post("/sign-out", (req, res, next) => {
   req.logout();
   res.json({ user: {} });
 });
